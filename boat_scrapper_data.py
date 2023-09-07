a0e55b463f08f5345fb134fb1b93cf068d33e422
jaime7981 <jmfernandez1@miaundes.cl>
2023-09-07 20:23:41 -0300

    web scrappe data into dataframe

--- boat_scrapper_data.py
+++ boat_scrapper_data.py
@@ -20,9 +20,21 @@ def get_boat_data(driver, url):
     boat_data_div = driver.find_element(By.CLASS_NAME, 'table-light')
     boat_data_rows = boat_data_div.text.split('\n')
 
+    boat_data = {}
+
+    header = []
+    data = []
+
     for row in boat_data_rows:
         row_data = row.split(':')
-        print(row_data)
+        header.append(row_data[0])
+        data.append(row_data[1])
+        boat_data[row_data[0]] = row_data[1]
+
+    df_boat_data = pandas.DataFrame(boat_data, index=[0])
+
+    print(df_boat_data)
+    return df_boat_data
 
 
 def main():
@@ -32,9 +44,23 @@ def main():
 
     boat_urls = read_boats_urls('boats_urls.csv')
 
-    for url in boat_urls:
-        get_boat_data(driver, url)
+    dataframe_list = []
 
+    counter = 0
+
+    for url in boat_urls:
+        boat_df = get_boat_data(driver, url)
+        dataframe_list.append(boat_df)
+
+        counter += 1
+
+        if counter > 5:
+            break
+
+    df = pandas.concat(dataframe_list, ignore_index=True)
+    
+    print(df)
+    df.to_csv('all_boats_data.csv', index=False)
 
 if __name__ == '__main__':
     main()
